e4277cd0d4dbe343830f6e12791d3a2d2ae77e79
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-03-19 15:14:18 +0100

    add pagination

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import useFetch from "../hooks/useFetch";
 
 type Planet = {
   name: string;
@@ -24,7 +23,17 @@ type ResponsePlanets = {
 };
 
 const PlanetList: React.FC = () => {
-  const { data } = useFetch<ResponsePlanets>("https://swapi.dev/api/planets");
+  const [data, setData] = useState<ResponsePlanets | null>(null);
+
+  const fetchData = async (url: string) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchData("https://swapi.dev/api/planets/");
+  }, []);
 
   return (
     <div>
@@ -34,6 +43,36 @@ const PlanetList: React.FC = () => {
           <li key={planet.url}>{planet.name}</li>
         ))}
       </ul>
+      <button
+        onClick={() => {
+          if (data?.previous) {
+            fetchData(data.previous);
+          }
+        }}
+        disabled={!data?.previous}
+      >
+        Previous
+      </button>
+      {Array.from({ length: Math.ceil(data?.count / 10) }).map((_, index) => (
+        <button
+          key={index}
+          onClick={() => {
+            fetchData(`https://swapi.dev/api/planets/?page=${index + 1}`);
+          }}
+        >
+          {index + 1}
+        </button>
+      ))}
+      <button
+        disabled={!data?.next}
+        onClick={() => {
+          if (data?.next) {
+            fetchData(data.next);
+          }
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 };
